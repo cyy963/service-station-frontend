de0781969ef022669c88e8dcc40f4d98c8e62c99
cyrus_w <cyrusw89@gmail.com>
2024-06-24 20:25:40 +1200

    added filters and custom dropdowns

--- src/pages/FindStationPage.jsx
+++ src/pages/FindStationPage.jsx
@@ -3,7 +3,6 @@ import styles from "./FindStationPage.module.css"
 import StationData from "../components/Common/StationData";
 import Header from "../components/Common/Header";
 import Footer from "../components/Common/Footer";
-import map from "../assets/map.png"
 
 function FindStationPage() {
   return (
