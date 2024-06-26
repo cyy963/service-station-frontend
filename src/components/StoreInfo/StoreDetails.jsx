09740b6d645c61d151506c418604423f2803232a
SarahC233 <122423221+SarahC233@users.noreply.github.com>
2024-06-27 11:11:03 +1200

    some debugging for back link

--- src/components/StoreInfo/StoreDetails.jsx
+++ src/components/StoreInfo/StoreDetails.jsx
@@ -1,116 +1,26 @@
-// import React, { useEffect, useRef } from "react";
-// import { Loader } from "@googlemaps/js-api-loader";
-// import styles from "./StoreDetails.module.css";
-
-// const containerStyle = {
-//   width: "100%",
-//   height: "400px",
-// };
-
-// const StoreDetails = ({ store }) => {
-//   const mapRef = useRef(null);
-//   const mapInstanceRef = useRef(null);
-//   const markerRef = useRef(null);
-
-//   useEffect(() => {
-//     const loader = new Loader({
-//       apiKey: import.meta.env.VITE_GOOGLE_MAPS_KEY,
-//       version: "weekly",
-//       libraries: ["marker"], // Add the marker library
-//     });
-
-//     loader
-//       .load()
-//       .then(() => {
-//         if (store && mapRef.current) {
-//           const lat = parseFloat(store.latitude);
-//           const lng = parseFloat(store.longitude);
-
-//           if (!isNaN(lat) && !isNaN(lng)) {
-//             const position = { lat, lng };
-
-//             if (!mapInstanceRef.current) {
-//               mapInstanceRef.current = new window.google.maps.Map(
-//                 mapRef.current,
-//                 {
-//                   center: position,
-//                   zoom: 15,
-//                   mapId: "DEMO_MAP_ID", // default demo map ID
-//                 }
-//               );
-
-//               google.maps
-//                 .importLibrary("marker")
-//                 .then(({ AdvancedMarkerElement }) => {
-//                   markerRef.current = new AdvancedMarkerElement({
-//                     map: mapInstanceRef.current,
-//                     position: position,
-//                     title: store.name,
-//                   });
-//                 });
-//             } else {
-//               mapInstanceRef.current.setCenter(position);
-//               markerRef.current.position = position;
-//               markerRef.current.title = store.name;
-//             }
-//           } else {
-//             console.error("Invalid latitude or longitude values:", lat, lng);
-//           }
-//         }
-//       })
-//       .catch((e) => {
-//         console.error("Error loading Google Maps API", e);
-//       });
-//   }, [store]);
-
-//   if (!store) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div className={styles.storeDetails}>
-//       <h1>{store.name}</h1>
-//       <p>
-//         <strong>Address:</strong> {store.address}
-//       </p>
-//       <p>
-//         <strong>Hours:</strong> {store.hours}
-//       </p>
-//       <p>
-//         <strong>Services:</strong> {store.services}
-//       </p>
-//       <p>
-//         <strong>ZX Premium:</strong> ${store.ZX_Premium}
-//       </p>
-//       <p>
-//         <strong>Z91 Unleaded:</strong> ${store.Z91_Unleaded}
-//       </p>
-//       <p>
-//         <strong>Z Diesel:</strong> ${store.Z_Diesel}
-//       </p>
-//       <div className={styles.mapContainer}>
-//         <div ref={mapRef} style={containerStyle}></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default StoreDetails;
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./StoreDetails.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const StoreDetails = ({ store }) => {
   return (
     <div className={styles.storeDetails}>
+      <Link to="/find-station" className={styles.backLink}>
+        <FontAwesomeIcon icon={faChevronLeft} />
+        <span className={styles.backLinkText}>Back to find a station</span>
+      </Link>
       <h1>{store.name}</h1>
       <div className={styles.addressContainer}>
         <p className={styles.address}>{store.address}</p>
         <button className={styles.getDirectionsButton}>
           Get directions
           <FontAwesomeIcon
-            icon={faCircleArrowRight}
+            icon={faChevronRight}
             style={{ color: "white", marginLeft: "10px" }}
           />
         </button>
@@ -120,11 +30,3 @@ const StoreDetails = ({ store }) => {
 };
 
 export default StoreDetails;
-
-
-
-
-
-
-
-
--- src/pages/StoreInfoPage.jsx
+++ src/pages/StoreInfoPage.jsx
@@ -1,53 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { useParams } from "react-router-dom";
-// import StoreDetails from "../components/StoreInfo/StoreDetails";
-// import styles from "./StoreInfoPage.module.css";
-// import Header from "../components/Common/Header";
-// import Footer from "../components/Common/Footer";
-
-// const StoreInfoPage = () => {
-//   const { id } = useParams();
-//   const [store, setStore] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchStoreData = async () => {
-//       try {
-//         const response = await axios.get(
-//           `http://localhost:3000/api/stations/${id}`
-//         );
-//         setStore(response.data);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Error fetching the store data", error);
-//         setError(error);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchStoreData();
-//   }, [id]);
-
-//   if (loading) {
-//     return <p>Loading store details...</p>;
-//   }
-
-//   if (error) {
-//     return <p>Error loading store details: {error.message}</p>;
-//   }
-
-//   return (
-//     <div className={styles.storeInfoPage}>
-//       <Header />
-//       {store ? <StoreDetails store={store} /> : <p>Store details not found.</p>}
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default StoreInfoPage;
 import React, { useEffect, useState } from "react";
 import styles from "./StoreInfoPage.module.css";
 import Header from "../components/Common/Header";
@@ -127,6 +77,3 @@ const StoreInfoPage = () => {
 };
 
 export default StoreInfoPage;
-
-
-
